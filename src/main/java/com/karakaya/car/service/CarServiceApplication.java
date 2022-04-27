84a58b18eceed508a25a3a79415dc7af665b9195
TCOKARAKAYA <onur.karakaya@turkcell.com.tr>
2022-04-27 23:10:03 +0300

    Initial commit

---
+++ src/main/java/com/karakaya/car/service/CarServiceApplication.java
@@ -0,0 +1,15 @@
+package com.karakaya.car.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CarServiceApplication.class, args);
+    }
+
+}
